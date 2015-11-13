668cf1025635f9da18585965e68b39d3e1b2c365
Sathvik Kadaveru <Sathvik@lawn-143-215-122-117.lawn.gatech.edu>
2015-11-12 19:27:30 -0500

    Parts 1-4

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,6 +7,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
     
+    @IBAction func addButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func subtractButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func multiplyButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func divideButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func equalsButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func clearButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func numberButtonPressed(sender: UIButton) {
+    }
 }
 
