64548c7a76e7ceac2b03c8c52c0230d05d537a94
Sathvik Kadaveru <Sathvik@res380d-128-61-83-219.res.gatech.edu>
2015-11-14 23:13:13 -0500

    Finished Basics

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,27 +7,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let calculations: CalculationDelegate = DefaultCalculations()
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func addButtonPressed(sender: UIButton) {
+        setOperator("+", withFunction: +)
     }
     
     @IBAction func subtractButtonPressed(sender: UIButton) {
+        setOperator("-", withFunction: -)
     }
     
     @IBAction func multiplyButtonPressed(sender: UIButton) {
+        setOperator("*", withFunction: *)
     }
     
     @IBAction func divideButtonPressed(sender: UIButton) {
+        setOperator("/", withFunction: /)
+    }
+    
+    func setOperator(character: String, withFunction function: (Double, Double) -> (Double)) {
+        //DefaultOperator is part of the default CalculationDelegate
+        let customOperator = DefaultOperator(forCharacter: character, withFunction: function)
+        calculations.setOperator(customOperator)
+        
+        //again, update the text on screen
+        resultLabel.text = calculations.resultNumber.roundedString
     }
     
     @IBAction func equalsButtonPressed(sender: UIButton) {
+        calculations.clearInputAndSave(true)
+        resultLabel.text = calculations.resultNumber.roundedString
     }
     
     @IBAction func clearButtonPressed(sender: UIButton) {
+        calculations.clearInputAndSave(false)
+        resultLabel.text = calculations.resultNumber.roundedString
     }
     
     @IBAction func numberButtonPressed(sender: UIButton) {
+        let buttonTitle = sender.titleLabel!.text!
+        let buttonNumber = Int(buttonTitle)! //converts String to Int
+        calculations.handleInput(buttonNumber)
+        
+        //also update the text on screen
+        resultLabel.text = calculations.resultNumber.roundedString
     }
 }
 
